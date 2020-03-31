805821db17b9dd8468481052339aded4a0825ff9
Jill Shah <jillrshah@gmail.com>
2020-03-31 15:02:08 -0400

    Updated with error messages

--- main.js
+++ main.js
@@ -11,12 +11,19 @@ $("body").on("keyup keydown keypress change", ".department input", function (e) 
 			running_total = running_total - user_input;
 		}
 	});
+
 	if (running_total <= 20 && running_total >= 0) {
 		$(".total_budget").addClass("warning").removeClass("error");
+		$(".advice.warning").addClass("active");
+		$(".advice.error").removeClass("active");
 	} else if (running_total < 0) {
 		$(".total_budget").addClass("error").removeClass("warning");
+		$(".advice.warning").removeClass("active");
+		$(".advice.error").addClass("active");
 	} else {
 		$(".total_budget").removeClass("error warning");
+		$(".advice.warning").removeClass("active");
+		$(".advice.error").removeClass("active");
 	}
 	$(".total_budget span").text(running_total);
 });
